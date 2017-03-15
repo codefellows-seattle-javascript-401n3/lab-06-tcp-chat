f810c9557271d4f412047dbc3903f466f426383c
Rubiksron <rubiksron@gmail.com>
2017-03-15 14:22:16 -0700

    I added three tests.

--- lab-ron/test/tcp-test.js
+++ lab-ron/test/tcp-test.js
@@ -3,12 +3,34 @@
 const assert = require('assert');
 const mocha = require('mocha');
 const server = require('../server.js');
+const net = require('net');
+const client = new net.Socket();
 
-describe('\\nick', function(){
-  describe('nickname changing', function() {
-    it('should change nickname', function(done) {
 
+describe('Testing the Connection to Server', function(done) {
+  it('should connect to localhost server', function() {
+    client.connect( { host: 'localhost', port: 3000 }, function() {
+      assert.equal(client);
       done();
     });
+  });
+});
+
+describe('testing if new user was created', function() {
+  it('should be equal', function(done) {
+    client.once('data', function(data) {
+      assert.equal(data, client, 'should be equal');
+    });
+    done();
+  });
+});
+
+describe('\\nick', function(){
+  it('should change nickname', function(done) {
+    client.once('data', function(data) {
+      assert.equal(data.toString(), '\n your name was changed to: Ron');
+    });
+    client.write('\\nick Ron');
+    done();
   });
 });
